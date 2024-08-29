f3b974d806710c60c5330534474d11279131a2e4
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-08-29 23:58:38 +0530

    recursion

--- searching.js
+++ searching.js
@@ -9,4 +9,16 @@ function search(arr,target) {
 }
 
 let arr = [1, 2, 4, 5];
-console.log(search(arr, 5))
+console.log(search(arr, 5));
+
+
+
+function factorialRecursive(n) {
+    if (n === 0 || n === 1) {
+      return 1; // Base case
+    }
+    return n * factorialRecursive(n - 1); // Recursive case
+  }
+  
+  console.log(factorialRecursive(5)); // Output: 120
+  
