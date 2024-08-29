b4e7d6ddc665fff5dc3bc24b93614bc7b1587dbb
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-08-29 23:59:04 +0530

    bubble sort

--- searching.js
+++ searching.js
@@ -20,5 +20,20 @@ function factorialRecursive(n) {
     return n * factorialRecursive(n - 1); // Recursive case
   }
   
-  console.log(factorialRecursive(5)); // Output: 120
+console.log(factorialRecursive(5)); // Output: 120
+  
+
+
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+    return arr;
+  }
+  
   
