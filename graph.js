d9b2998f1527d60dd815bacb496f64051d61be2b
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-08-30 23:53:25 +0530

    graph implemented

--- graph.js
+++ graph.js
@@ -12,10 +12,34 @@ class Graph{
 
 
     addEdge(v1, v2) {
-        
+        if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+            this.adjacencyList[v1].push(v2);
+            this.adjacencyList[v2].push(v1);
+        }
+    }
+
+    print() {
+        for (let vertex in this.adjacencyList) {
+            console.log(`${vertex} --> ${this.adjacencyList[vertex]}`);
+        }
     }
 }
 
+
+const g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+
+g.addEdge("A", "B");
+g.addEdge("B", "C");
+g.addEdge("C", "D");
+g.addEdge("D", "E");
+g.addEdge("E", "A");
+
+g.print();
 
 
 
