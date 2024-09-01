01c66d4b85b274e696a46e729c0ae2ac183e8f96
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-01 13:55:02 +0530

    bfs implemented in graph

--- graph.js
+++ graph.js
@@ -18,6 +18,23 @@ class Graph{
         }
     }
 
+    bfs(startingVertex) {
+        const queue = [startingVertex];
+        const visited = {};
+        visited[startingVertex] = true;
+        while (queue.length > 0) {
+            const currentVertex = queue.shift();
+            console.log(currentVertex);
+
+            this.adjList[currentVertex].forEach(element => {
+                if (!visited[element]) {
+                    visited[element] = true;
+                    queue.push(element);
+                }
+            });
+        }
+    }
+
     print() {
         for (let vertex in this.adjList) {
             console.log(`${vertex} ---> ${this.adjList[vertex].join(',')}`);
