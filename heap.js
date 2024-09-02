02901fba2313289c81196c7b3058801bdf780737
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-02 23:41:03 +0530

    heap implementing started

--- heap.js
+++ heap.js
@@ -1,2 +1,10 @@
+class MinHeap {
+  constructor() {
+      this.heap = [];
+  }
 
+  // Get the index of the parent of the element at index i
+  parentIndex(i) {
+      return Math.floor((i - 1) / 2);
+  }
   
