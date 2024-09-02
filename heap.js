787d6a81db9f3b99f14c6e899b5aa390dc21f334
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-02 23:41:39 +0530

    heap inserting

--- heap.js
+++ heap.js
@@ -7,4 +7,23 @@ class MinHeap {
   parentIndex(i) {
       return Math.floor((i - 1) / 2);
   }
-  
+   // Get the index of the left child of the element at index i
+   leftChildIndex(i) {
+    return 2 * i + 1;
+}
+
+// Get the index of the right child of the element at index i
+rightChildIndex(i) {
+    return 2 * i + 2;
+}
+
+// Swap two elements in the heap
+swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+}
+
+// Insert a new element into the heap
+insert(key) {
+    this.heap.push(key);
+    this.heapifyUp(this.heap.length - 1);
+}
