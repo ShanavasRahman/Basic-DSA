50bba48387ab72d3888c57fded62b90b3802e8a3
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-03 23:35:01 +0530

    extract min

--- heap.js
+++ heap.js
@@ -27,10 +27,23 @@ insert(key) {
     this.heap.push(key);
     this.heapifyUp(this.heap.length - 1);
     }
-    
+
     heapifyUp(i) {
         while (i > 0 && this.heap[this.parentIndex(i)] > this.heap[i]) {
             this.swap(i, this.parentIndex(i));
             i = this.parentIndex(i);
         }
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+        const root = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown(0);
+        return root;
     }
