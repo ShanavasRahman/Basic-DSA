f8c30b9b28b3439daf2bcac2b431ae7205fd14ca
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-05 23:04:33 +0530

    question 1

--- object.js
+++ object.js
@@ -1,8 +1,19 @@
-//How do you create an object in JavaScript?
+// //How do you create an object in JavaScript?
+
+// let person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+//   };
+  
+
+//   What is the this keyword in JavaScript objects, and how is it used? this refers to the object that is executing the current function.
 
 let person = {
     name: "John",
-    age: 30,
-    city: "New York"
+    greet: function() {
+      console.log("Hello, " + this.name);
+    }
   };
+  person.greet(); // "Hello, John"
   
