46e8ac3a3809641986d09570227a318d1df99019
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-07 23:52:52 +0530

    deletion

--- rerevision.js
+++ rerevision.js
@@ -1,61 +1,58 @@
 //Linked List
 
-
-class Node{
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-class LinkedList{
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-    append(data) {
-        const newNode = new Node(data);
+  append(data) {
+    const newNode = new Node(data);
 
-        if (this.head == null) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    prepend(data) {
-        const newNode = new Node(data);
-        if (this.head == null) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
+  }
+  prepend(data) {
+    const newNode = new Node(data);
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
+  }
 
-    deleteNode(target) {
-        let current = this.head;
-        let prev = null;
+  deleteNode(target) {
+    let current = this.head;
+    let prev = null;
 
-        while (current) {
-            if (current.value == target) {
-                
-            }
-        }
+    while (current) {
+      if (current.value == target) {
+      }
     }
-    print() {
-        let current = this.head;
-
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
+  }
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
     }
+  }
 }
 
 const list = new LinkedList();
@@ -64,4 +61,4 @@ list.prepend(20);
 list.prepend(30);
 list.prepend(40);
 list.prepend(50);
-list.print()
+list.print();
