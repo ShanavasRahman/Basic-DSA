134fc0e5426a6d457f3d56e50df508e9e9b04e0b
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-10 13:17:58 +0530

    learning heap

--- heap.js
+++ heap.js
@@ -1,96 +1,35 @@
-class MinHeap {
-  constructor() {
-      this.heap = [];
-  }
+class Heap{
+    constructor() {
+        this.heap = [];
+    }
 
-  // Get the index of the parent of the element at index i
-  parentIndex(i) {
-      return Math.floor((i - 1) / 2);
-  }
-   // Get the index of the left child of the element at index i
-   leftChildIndex(i) {
-    return 2 * i + 1;
+    getParent(index) {
+        return Math.floor((index - 1) / 2);
+    }
+    getLeftChild(index) {
+        return 2 * index + 1;
+    }
+    getRightChild(index) {
+        return 2 * index + 2;
+    }
+    swap(index1, index2) {
+        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    }
+
+
+    insert(value) {
+        this.heap.push(value);
+        this.heapifyUp(this.heap.length - 1);
+    }
+
+    heapifyUp(index) {
+        let parentIndex = this.getParent(index);
+        // In a max-heap, we check if the current node is greater than the parent
+        while (index > 0 && this.heap[index] > this.heap[parentIndex]) {
+            this.swap(index, parentIndex);  // Swap the current node with the parent
+            index = parentIndex;  // Move up to the parent's position
+            parentIndex = this.getParent(index);  // Get the parent's parent index
+        }
+    }
+    
 }
-
-// Get the index of the right child of the element at index i
-rightChildIndex(i) {
-    return 2 * i + 2;
-}
-
-// Swap two elements in the heap
-swap(i, j) {
-    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-}
-
-// Insert a new element into the heap
-insert(key) {
-    this.heap.push(key);
-    this.heapifyUp(this.heap.length - 1);
-    }
-
-    heapifyUp(i) {
-        while (i > 0 && this.heap[this.parentIndex(i)] > this.heap[i]) {
-            this.swap(i, this.parentIndex(i));
-            i = this.parentIndex(i);
-        }
-    }
-
-    extractMin() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        if (this.heap.length === 1) {
-            return this.heap.pop();
-        }
-        const root = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown(0);
-        return root;
-    }
-    heapifyDown(i) {
-        let smallest = i;
-        const left = this.leftChildIndex(i);
-        const right = this.rightChildIndex(i);
-
-        if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
-            smallest = left;
-        }
-
-        if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
-            smallest = right;
-        }
-
-        if (smallest !== i) {
-            this.swap(i, smallest);
-            this.heapifyDown(smallest);
-        }
-    }
-    peek() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        return this.heap[0];
-    }
-
-    // Convert an array into a heap
-    heapify(array) {
-        this.heap = array;
-        for (let i = Math.floor(this.heap.length / 2); i >= 0; i--) {
-            this.heapifyDown(i);
-        }
-    }
-}
-
-// Usage example
-const minHeap = new MinHeap();
-minHeap.insert(3);
-minHeap.insert(1);
-minHeap.insert(6);
-minHeap.insert(5);
-minHeap.insert(2);
-minHeap.insert(4);
-
-console.log(minHeap.extractMin()); // 1
-console.log(minHeap.extractMin()); // 2
-console.log(minHeap.extractMin()); // 3
-console.log(minHeap.peek());  
