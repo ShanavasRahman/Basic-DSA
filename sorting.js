1da29c7a5d07a9a22505369619dc7d86a77695a4
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-20 22:00:27 +0530

    insertion sort implemented

--- sorting.js
+++ sorting.js
@@ -1,12 +1,28 @@
 //Bubble sort
 
+// let arr = [5, 3, 7, 2, 8, 6];
+
+// for (let i = 0; i < arr.length; i++){
+//     for (j = 0; j < arr.length-i; j++){
+//         if (arr[j] > arr[j+1]) {
+//             [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+//         }
+//     }
+// }
+// console.log(arr);
+
+
+
+//Insertion sort
 let arr = [5, 3, 7, 2, 8, 6];
 
-for (let i = 0; i < arr.length; i++){
-    for (j = 0; j < arr.length-i; j++){
-        if (arr[j] > arr[j+1]) {
-            [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
-        }
+for (let i = 1; i < arr.length; i++){
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+        arr[j + 1] = arr[j];
+        j--;
     }
+    arr[j + 1] = temp;
 }
 console.log(arr);
