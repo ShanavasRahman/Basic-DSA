031462a5c63ba6aa1cc2b499dd2c8181cd900b99
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-20 22:44:01 +0530

    selection sort implemented

--- sorting.js
+++ sorting.js
@@ -13,16 +13,33 @@
 
 
 
-//Insertion sort
+// //Insertion sort
+// let arr = [5, 3, 7, 2, 8, 6];
+
+// for (let i = 1; i < arr.length; i++){
+//     let temp = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > temp) {
+//         arr[j + 1] = arr[j];
+//         j--;
+//     }
+//     arr[j + 1] = temp;
+// }
+// console.log(arr);
+
+
+
+//selection sort
+
 let arr = [5, 3, 7, 2, 8, 6];
 
-for (let i = 1; i < arr.length; i++){
-    let temp = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > temp) {
-        arr[j + 1] = arr[j];
-        j--;
+for (let i = 0; i < arr.length; i++){
+    let min = i;
+    for (let j = i+1; j < arr.length; j++){
+        if (arr[j] < arr[min]) {
+            min = j;
+        }
     }
-    arr[j + 1] = temp;
+    [arr[i],arr[min]] = [arr[min], arr[i]];
 }
 console.log(arr);
