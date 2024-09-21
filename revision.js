4eb09c9be476a2895f3ef6b21bc5e01326094bbe
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-21 15:36:33 +0530

    bubble revisioned

--- revision.js
+++ revision.js
@@ -371,15 +371,28 @@
 
 //insertion sort
 
+// let arr = [5, 6, 3, 8, 2];
+// for (let i = 1; i < arr.length; i++){
+//     let temp = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > temp) {
+//         arr[j + 1] = arr[j];
+//         j--;
+//     }
+//     arr[j + 1] = temp;
+// }
+// console.log(arr);
+
+
+//bubble sort
+
 let arr = [5, 6, 3, 8, 2];
-for (let i = 1; i < arr.length; i++){
-    let temp = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > temp) {
-        arr[j + 1] = arr[j];
-        j--;
+for (i = 0; i < arr.length; i++){
+    for (j = 0; j < arr.length - i; j++){
+        if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
     }
-    arr[j + 1] = temp;
 }
 console.log(arr);
 
