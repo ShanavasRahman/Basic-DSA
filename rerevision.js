d4df0573f4ed48cd45e1c2999a8815cd85ce788c
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-22 23:55:03 +0530

    rerevisioned selectionsort

--- rerevision.js
+++ rerevision.js
@@ -330,8 +330,15 @@
 let arr = [3, 2, 5, 6, 1, 4];
 
 for (let i = 0; i < arr.length; i++){
-    let min=
+    let min = i;
+    for (let j = i+1; j < arr.length; j++){
+        if (arr[j] < arr[min]) {
+            min = j;
+        }
+    }
+    [arr[i], arr[min]] = [arr[min], arr[i]];
 }
+console.log(arr);
 
 
 
