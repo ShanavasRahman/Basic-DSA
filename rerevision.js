61b66c09d6f6623fb391ec4aa4e328214031c7e9
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-22 23:50:05 +0530

    rerevision selectionsort

--- rerevision.js
+++ rerevision.js
@@ -312,19 +312,26 @@
 
 // insertion sort
 
+// let arr = [3, 2, 5, 6, 1, 4];
+
+// for (let i = 1; i < arr.length; i++){
+//     let temp = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > temp) {
+//         arr[j + 1] = arr[j];
+//         j--;
+//     }
+//     arr[j + 1] = temp;
+// }
+// console.log(arr);
+
+
+//selection sort
 let arr = [3, 2, 5, 6, 1, 4];
 
-for (let i = 1; i < arr.length; i++){
-    let temp = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > temp) {
-        arr[j + 1] = arr[j];
-        j--;
-    }
-    arr[j + 1] = temp;
+for (let i = 0; i < arr.length; i++){
+    let min=
 }
-console.log(arr);
-
 
 
 
