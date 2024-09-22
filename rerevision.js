00782e2e88dfcc858603dd47f9c6e584c5fa30c5
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-09-22 23:48:25 +0530

    rerevisioned insertionsort

--- rerevision.js
+++ rerevision.js
@@ -317,8 +317,13 @@ let arr = [3, 2, 5, 6, 1, 4];
 for (let i = 1; i < arr.length; i++){
     let temp = arr[i];
     let j = i - 1;
-    while(j>=0 && arr[j]>temp)
+    while (j >= 0 && arr[j] > temp) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = temp;
 }
+console.log(arr);
 
 
 
