e74d7af13a0498a5959a430ef16a6708bc82dbed
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-10-01 00:10:52 +0530

    dsa modified

--- rerevision.js
+++ rerevision.js
@@ -1,4 +1,24 @@
+//Graph
 
+
+class Graph{
+    constructor() {
+        this.adjList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjList[vertex]) {
+            this.adjList[vertex] = [];
+        }
+    }
+    addEdge(v1, v2) {
+        if (this.adjList[v1] && this.adjList[v2]) {
+            this.adjList[v1].push(v2);
+            this.adjList[v2].push(v1);
+        }
+    }
+
+}
 
 
 
