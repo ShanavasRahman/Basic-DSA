c33f3cdeb95b355cd913d2bfcbcdabeb5956ec40
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-10-01 17:29:38 +0530

    revision is going on

--- array.js
+++ array.js
@@ -1,18 +1,14 @@
-
-
 //shift
 
 // let arr = [4, 2, 3, 1];
 // arr.shift();
 // console.log(arr);
 
-
 //unShift
 
 // let arr = [4, 2, 3, 1];
 // arr.unshift(5);
 // console.log(arr);
-
 
 //concat
 
@@ -21,13 +17,11 @@
 // let arrSam=arr.concat(newArr);
 // console.log(arrSam);
 
-
 //slice(starting index(included), ending index(not included))
 
 // let arr = [4, 6, 7, 8, 3, 2, 4, 6];
 // let newArr = arr.slice(2, 6);
 // console.log(newArr);
-
 
 //splice(index to start changing, delete count,elements to add)
 
@@ -66,7 +60,6 @@
 
 // splitIntoChunks(arr, 3);
 
-
 // let obj = {
 //     a: 10,
 //     b: 20,
@@ -88,10 +81,7 @@
 //     }
 // })
 
-
-
 //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
 
 // const nestedObj = {
 //     a: 1,
@@ -107,23 +97,75 @@
 // function flattingObj(obj) {
 //     for (let key in obj) {
 //         if (obj[key] == "object" && obj[key] != null) {
-           
+
 //         }
 //     }
 // }
 
+// let arr = "12dg56";
+// let removed = arr.split('').filter((x) => /[a-zA-Z]/.test(x)).join('');
+// console.log(removed);
 
-let arr = [3, 4, 5, 6, 3, 9, 5];
-let newArr=[]
-for (let i = 0; i < arr.length; i++){
-    if (arr[i] % 2 != 0) {
-        for (let j = i + 1; j < arr.length; j++){
-            if (arr[i] == arr[j]) {
-                newArr.push(arr[i]);
-            }
-        }
-    }
-}
+// Find the Missing Number in an Array
 
-console.log(newArr);
+// Problem: You are given an array of n-1 integers, where the integers are in the range of 1 to n. One number is missing. Find the missing number.
+// Example:
+// Input: [1, 2, 4, 5, 6]
+// Output: 3
 
+// let arr = [1, 2, 4, 5, 6];
+// function missing(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i + 1 != arr[i]) {
+//            return i + 1;
+//         }
+//     }
+// }
+// let result = missing(arr);
+// console.log(result);
+
+// Find Duplicates in an Array
+
+// Problem: Given an array of integers, find all the duplicate elements.
+// Example:
+// Input: [4, 3, 2, 7, 8, 2, 3, 1]
+
+// let arr = [4, 3, 2, 7, 8, 2, 3, 1];
+
+// let count = {};
+// let newArr=[]
+
+// for (let i = 0; i < arr.length; i++){
+//     if (!count[arr[i]]) {
+//         count[arr[i]] = 1;
+//     } else {
+//         count[arr[i]]++;
+//     }
+// }
+// for (let key in count) {
+//     if (count[key] > 1) {
+//         newArr.push(key);
+//     }
+// }
+// console.log(newArr);
+
+// let obj = { name: 'hisham', email: 'hisham@gmail.com' }
+
+// let result = '';
+// for (let key in obj) {
+//     result += `${key}=${obj[key]} `;
+// }
+// console.log(result);
+
+let arr = [5, 20, 2, 6];
+let first = 0;
+let second = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > first) {
+    second = first;
+    first = arr[i];
+  } else if (arr[i] < first && arr[i] > second) {
+    second = arr[i];
+  }
+};
+console.log(second);
