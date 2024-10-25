5adc435bfe3dd170dbe7e54a33300b3de88d0fd0
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-10-25 23:49:36 +0530

    modifying

--- object.js
+++ object.js
@@ -80,7 +80,18 @@ function flatting(obj, parentKey, result) {
     return result;
 }
   
+let result = flatting(obj);
+console.log(result);
 
+
+let a = {};
+let b = { key: "b" };
+let c = { key: "c" };
+
+a[b] = 123;
+a[c] = 456;
+
+console.log(Object.entries(a));
 
 
 
