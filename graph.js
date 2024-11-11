9a1fb44953957309a6ea560750d9f3a5f9f0abd1
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-11-11 23:40:08 +0530

    graph

--- graph.js
+++ graph.js
@@ -54,7 +54,11 @@ class Graph{
         }
     }
 
-
+    print() {
+        for (let vertex in this.adjList) {
+            console.log(`${vertex} ---> ${this.adjList[vertex].join(',')}`);
+        }
+    }
 }
 
 
