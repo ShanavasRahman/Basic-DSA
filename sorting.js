ce3dd0b600fc9c2808c1d3785e9e5bc515d32605
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-12-06 22:26:11 +0530

    modifying

--- sorting.js
+++ sorting.js
@@ -13,7 +13,19 @@
 
 
 
+// //Insertion sort
+// let arr = [5, 3, 7, 2, 8, 6];
 
+// for (let i = 1; i < arr.length; i++){
+//     let temp = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > temp) {
+//         arr[j + 1] = arr[j];
+//         j--;
+//     }
+//     arr[j + 1] = temp;
+// }
+// console.log(arr);
 
 
 
