c86885de511dfc4a299b26c6cbacbe8fcb6bb739
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-12-06 22:25:31 +0530

    modifying

--- sorting.js
+++ sorting.js
@@ -48,6 +48,23 @@
 
 // let a = [5, 6, 3, 8, 2];
 
+function quickSort(a) {
+    if (a.length <= 1) {
+        return a
+    }
+    let pivot = a[0];
+    let left = [];
+    let right = [];
 
+    for (let i = 1; i < a.length; i++){
+        if (a[i] < pivot) {
+            left.push(a[i]);
+        } else {
+            right.push(a[i]);
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+    
+}
 
 // console.log(quickSort(a));
