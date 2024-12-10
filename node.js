b777c098ad4cbcec5655e1144b7f4479be5bbd8c
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-12-10 23:16:49 +0530

    node is inside

--- node.js
+++ node.js
@@ -1,6 +1,23 @@
 const fs = require('fs');
 
+// fs.writeFile('example.txt', "hai this is shanavas", () => {
+//     console.log("file created successfully")
+// })
 
+// fs.readFile('example.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data);
+
+// })
+
+// fs.appendFile('example.txt', "this is adding", (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log("added successfully")
+// })
 
 const currDate = new Date().toLocaleDateString();
 const currTime = new Date().toLocaleTimeString();
