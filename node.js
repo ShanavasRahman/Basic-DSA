aa50947d460dc94c475b1a719d544d3a4fae5796
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-12-10 23:16:33 +0530

    node is inside

--- node.js
+++ node.js
@@ -22,3 +22,9 @@ const fs = require('fs');
 const currDate = new Date().toLocaleDateString();
 const currTime = new Date().toLocaleTimeString();
 
+fs.writeFile('sample.js',"hello world", (err) =>{
+    if(err) {
+        console.log(err);
+    }
+    console.log("file created sucessfully");
+})
