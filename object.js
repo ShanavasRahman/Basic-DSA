7677772a07cbf46ee82f1b3cb125240867b25c5f
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-12-18 22:35:27 +0530

    practicing

--- object.js
+++ object.js
@@ -84,7 +84,14 @@ let result = flatting(obj);
 console.log(result);
 
 
+let a = {};
+let b = { key: "b" };
+let c = { key: "c" };
 
+a[b] = 123;
+a[c] = 456;
+
+console.log(Object.entries(a));
 
 
 
