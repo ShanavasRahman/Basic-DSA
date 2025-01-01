956c511298a8ea627ec19ce71699b1861107b382
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2025-01-01 23:47:52 +0530

    modifying

--- rerevision.js
+++ rerevision.js
@@ -217,6 +217,26 @@
 
 //quick sort
 
+let arr = [5, 3, 6, 2, 7, 4];
 
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+let result = quickSort(arr);
+console.log(result);
 
 
