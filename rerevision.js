858497731b362715c0c5914caa8f8d888405aa0a
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2025-01-01 23:48:12 +0530

    modifying

--- rerevision.js
+++ rerevision.js
@@ -201,7 +201,18 @@
         // }
         // console.log(arr);
         
+// let arr = [5, 3, 6, 2, 7, 4];
 
+// for (let i = 0; i < arr.length; i++){
+//     let min = i;
+//     for (let j = i + 1; j < arr.length; j++){
+//         if (arr[min] > arr[j]) {
+//             min = j;
+//         }
+//     }
+//     [arr[i], arr[min]] = [arr[min], arr[i]];
+// }
+// console.log(arr);
 
 
 //quick sort
