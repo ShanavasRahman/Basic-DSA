9ef914ab3e202f0d5ff1b834bcb7f89735691e7d
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2025-01-01 23:48:25 +0530

    modifying

--- rerevision.js
+++ rerevision.js
@@ -190,16 +190,7 @@
 
 //Insertion sort
 
-// for (let i = 1; i < arr.length; i++){
-    //     let temp = arr[i];
-    //     let j = i - 1;
-    //     while (j >= 0 && arr[j] > temp) {
-        //         arr[j + 1] = arr[j];
-        //         j--;
-        //     }
-        //     arr[j + 1] = temp;
-        // }
-        // console.log(arr);
+
         
 // let arr = [5, 3, 6, 2, 7, 4];
 
